0377d62c5fe5ec1c9d25971d63b70435be2b6711
AmberAutumnsky <autumnroseskye@pm.me>
2026-03-22 10:28:49 +1030

    cleanup

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -33,59 +33,11 @@ export default function Board(props: BoardProps): JSX.Element {
     for (let col = 0; col < NUM_COLUMNS; col++) {
       for (let row = 0; row < NUM_ROWS; row++) {
         const i = col * NUM_ROWS + row;
-        // todo
-        //  - build coordinate type
-        //  - use coord type to evaluate tile colour
-        // build coord type with tests yay
         grid.push(<Tile key={i} coordinate={new Coordinate(SQUARES[i])}/>);
       }
     }
-
-    // for (let y = 0; y < BOARD_SIZE; y++) {
-    //   for (let x = 0; x < BOARD_SIZE; x++) {
-    //     grid.push(
-    //       <Tile
-    //         key={`${x}:${y}`}
-    //         color={colorOfTile(x, y)} 
-    //         coord={coordOfTile(x, y)}
-    //         posX={x}
-    //         posY={y}
-    //         onClick={onTileClicked}
-    //       >
-    //         {}
-    //       </Tile>
-    //     )
-    //   }    
-    // }
     return grid;
   }
-
-  
-  // function colorOfTile(coord: string): TileProps["color"] {
-  //   const s = coord.split('');
-  //   const row = s[0];
-  //   const col = s[1];
-
-  //   if ()
-
-  //   if (x % 2 === 0) {
-  //     return y % 2 === 0 ? "white" : "black";
-  //   } else {
-  //     return y % 2 === 0 ? "black" : "white";
-  //   } 
-  // }
-
-  // function coordOfTile(x: number, y: number): string {
-  //   const columnLookup = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-  //   const rowLookup = ['8','7','6','5','4','3','2','1'];
-  //   return columnLookup[x] + rowLookup[y];
-  // }
-
-  // function onTileClicked(x: number, y: number) {
-  //   console.log('clicked a tile')
-  //   // todo: check necessary api for chess plugin
-  //   console.log(`${x}:${y}`)
-  // }
 
   let coord = new Coordinate("A8");
 
--- src/components/tile.tsx
+++ src/components/tile.tsx
@@ -12,28 +12,11 @@ export interface TileProps {
 
 export default function Tile(props: TileProps): JSX.Element {
 
-
-  // todo: tile resolves own colour
-  // REMEMBER THAT EVERYTHING SHOULD BE ENGINEERED AROUND CLASSIC 8x8 CHESS
-  // DON'T GO GOOGLY EYED THINKING YOU CAN MAKE IT GENERIC FOR A FUTURE THAT WON'T BE IMPLEMENTED
-
-  // todo: tile instantiates own piece icon if data is present
-  // data should be passed thrugh props, or the piece itself as a child object?
-  // choose one
   return (
     <div className={`tile ${props.coordinate.colour()}`}>
       <span>{props.coordinate.string}</span>
       <span>{props.coordinate.column}</span>
       <span>{props.coordinate.row}</span>
     </div>
-    // <div 
-    //   className="tile"
-    //   style= {{
-    //     backgroundColor: props.color === 'black' ? "#111" : "#EEE"
-    //   }}
-    //   onClick={() => props.onClick(props.posX, props.posY)}
-    // >
-    //   <span>{props.coord}</span>
-    // </div>
   )
 }
